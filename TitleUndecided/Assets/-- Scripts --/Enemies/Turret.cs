ab7fc502e1cf1ede05a41c41028406066a4e9e02
Sid Greenwood <green272@msu.edu>
2025-04-01 01:03:25 -0400

    Turret

--- TitleUndecided/Assets/-- Scripts --/Enemies/Turret.cs
+++ TitleUndecided/Assets/-- Scripts --/Enemies/Turret.cs
@@ -1,28 +1,53 @@
 using UnityEngine;
 using UnityEngine.ProBuilder;
+using UnityEngine.Serialization;
 
 public class Turret : MonoBehaviour
 {
-    [Range(0, 1)] public float lookSpeed = 0.3f;
-    public static GameObject movewall;
-    public GameObject Barrel;
-    public GameObject Base;
-    public Transform playerpos;
-    public float distancetoplayer;
-    public float dist = 10f;
-    public Vector2 viewRange = new Vector2(45f, 45f);
-    public Vector3 startingRot;
+    [Header("References")]
     
-    private bool ranonce = false;
+    public static GameObject movewall;
+    
+    public GameObject Barrel;
+    
+    public GameObject Base;
+    
+    public Transform playerpos;
+    
+    [Header("Detection Settings")]
+    
+    public float dist = 10f;
 
+    public Vector2 viewRadius = new Vector2(45f, 45f);
+    
+    [Range(0, 1)] public float lookSpeed = 0.3f;
+    
+    [Header("Behaviour Settings")]
+    
+    public bool returnsToStartRot = true;
+    
+    [Header("Dynamic")]
+    
+    public float distancetoplayer;
+    
+    public Quaternion startingRot;
+    
+    [FormerlySerializedAs("targetRot")]
+    public Quaternion currentTargetRot;
+    
+    //Non serialized or private below
+    private bool ranonce = false;
     private Vector3 movedWall;
     private Vector3 movedBase;
     private Vector3 movedBarrel;
-    private Vector3 targetlookDir;
+    
   
 
     void Start()
     {
+        // viewRadius.x = Mathf.Clamp(viewRadius.x, 0, 179);
+        // viewRadius.y = Mathf.Clamp(viewRadius.y, 0, 179);
+        
         movewall = GameObject.Find("movewall");
         Barrel = GameObject.Find("barrel");
         Base = GameObject.Find("base");
@@ -31,7 +56,6 @@ public class Turret : MonoBehaviour
         movedBase = new Vector3(movewall.transform.position.x, Base.transform.position.y, Base.transform.position.z);
         movedBarrel = new Vector3(Barrel.transform.position.x + 1.6f, Barrel.transform.position.y,
             Barrel.transform.position.z);
-        
     }
     
 
@@ -53,85 +77,116 @@ public class Turret : MonoBehaviour
         {
             if (!ranonce)
             {
-                
-                startingRot = Barrel.transform.eulerAngles;
-                
-                if (startingRot.y > 180)
-                {
-                    startingRot.y -= 360;
-                    Debug.Log(startingRot.y);
-                }
 
-                if (startingRot.z > -180)
-                {
-                    startingRot.z -= 360;
-                    Debug.Log(startingRot.z);
-                }
+                startingRot = Barrel.transform.rotation;
+
                 ranonce = true;
             }
-            targetlookDir = startingRot;
+            
             StartCoroutine(Lerpers.LerpTransform(Base.transform, movedBase, Lerpers.OutQuad(0.7f)));
             if (Base.transform.position == movedBase)
-                
             {
                 StartCoroutine(Lerpers.LerpTransform(Barrel.transform, movedBarrel, Lerpers.OutQuad(0.3f)));
                 if (Barrel.transform.position == movedBarrel)
                 {
                     RotateBarrel();
-
                 }
 
             }
-
         }
-
     }
 
     void RotateBarrel()
-    
     {
-        Vector3 playerZOnly = new Vector3(playerpos.transform.position.x, playerpos.transform.position.y, playerpos.position.z);
-        float xRot = Barrel.transform.localEulerAngles.x;
-
-        Vector3 direction = playerpos.transform.position - Barrel.transform.position;
+        Vector3 targetDir = playerpos.transform.position - Barrel.transform.position;
         
-        targetlookDir = Quaternion.LookRotation(direction).eulerAngles;
+        Quaternion targetDirRot = Quaternion.LookRotation(targetDir);
         
+        Vector3 targetRotEuler = targetDirRot.eulerAngles;
         
-        if (targetlookDir.y > 180)
+        targetRotEuler = ClampEuler(startingRot.eulerAngles, targetRotEuler, viewRadius / 2);
+        
+        currentTargetRot = Quaternion.Euler(targetRotEuler);
+        
+        Barrel.transform.rotation = Quaternion.Slerp(Barrel.transform.rotation, currentTargetRot, lookSpeed);
+        
+        return;
+        
+        float ClampAngle(float angle, float minAngle, float maxAngle)
         {
-            targetlookDir.y -= 360;
+            angle = Mathf.Repeat(angle, 360f); 
+            
+            if (angle > 180f) {angle -= 360f;} 
+            
+            bool isBetween = angle > minAngle && angle < maxAngle;
+            
+            angle = Mathf.Clamp(angle, minAngle, maxAngle);
+            
+            if (isBetween) {return angle;}
+            else
+            {
+                if (returnsToStartRot)
+                {
+                    return 0;
+                }
+                else
+                {
+                    return angle;
+                }
+            }
         }
-        if (targetlookDir.x > 180)
+        
+        // float NewClampAngle(float dirOne, float dirTwo, float minAngle, float maxAngle, float fallbackAngle = -180)
+        // {
+        //     float angle = dirTwo - dirOne;
+        //     
+        //     angle = Mathf.Repeat(angle, 360f); 
+        //     
+        //     if (angle > 180f) {angle -= 360f;} 
+        //     
+        //     bool isBetween = angle > minAngle && angle < maxAngle;
+        //     
+        //     angle = Mathf.Clamp(angle, minAngle, maxAngle);
+        //     
+        //     if (isBetween) {return dirOne + angle;}
+        //     else
+        //     {
+        //         if (fallbackAngle <= -180)
+        //         {
+        //             return dirOne;
+        //         }
+        //         else
+        //         {
+        //             return fallbackAngle;
+        //         }
+        //     }
+        // }
+        
+        Vector3 ClampEuler(Vector3 startRot, Vector3 targetRot, Vector2 limits)
         {
-            targetlookDir.x -= 360;
+            Vector3 start = startRot;
+            
+            Vector3 target = targetRot;
+            
+            target.x = ClampAngle(target.x - start.x, -limits.y, limits.y) + start.x;
+            
+            target.y = ClampAngle(target.y - start.y, -limits.x, limits.x) + start.y;
+            
+            // if (returnsToStartRot)
+            // {
+            //     target.x = NewClampAngle(start.x, target.x, -limits.y, limits.y);
+            //
+            //     target.y = NewClampAngle(start.x, target.x , -limits.x, limits.x);
+            // }
+            // else
+            // {
+            //     target.x = NewClampAngle(start.x, target.x, -limits.y, limits.y, Barrel.transform.rotation.eulerAngles.x);
+            //
+            //     target.y = NewClampAngle(start.x, target.x , -limits.x, limits.x, Barrel.transform.rotation.eulerAngles.y);
+            // }
+            
+            return target;
         }
-        Debug.Log(targetlookDir.x);
-        
-        
-        
-        float ydiff = Mathf.Abs(startingRot.y) - Mathf.Abs(targetlookDir.y);
-        float zdiff = Mathf.Abs(startingRot.x) - Mathf.Abs(targetlookDir.x);
-        
-        // Debug.Log("y diff = " +ydiff);
-        // Debug.Log("z diff = " +zdiff);
-        //
-        if ((Mathf.Abs(ydiff) > viewRange.x ))
-        {
-            targetlookDir.y = startingRot.y;
-        }
-  
-        if ((Mathf.Abs(zdiff) > viewRange.y ))
-        {
-            targetlookDir.x = startingRot.x;
-        }
-        //targetlookDir.y = Mathf.Lerp(Barrel.transform.eulerAngles.y, targetlookDir.y, lookSpeed);
-        //targetlookDir.z = Mathf.Lerp(Barrel.transform.eulerAngles.z, targetlookDir.z, lookSpeed);
-      
-    
-        Barrel.transform.eulerAngles=  targetlookDir;
-
-
     }
     
 }
