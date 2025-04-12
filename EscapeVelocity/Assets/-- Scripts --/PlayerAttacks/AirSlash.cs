c503921a6721c6fb5f4bfc81dcb2aedd446c7f24
SovietSpy1 <michaelbiggar2005@outlook.com>
2025-04-11 22:14:49 -0400

    change

--- EscapeVelocity/Assets/-- Scripts --/PlayerAttacks/AirSlash.cs
+++ EscapeVelocity/Assets/-- Scripts --/PlayerAttacks/AirSlash.cs
@@ -6,17 +6,35 @@ public class AirSlash : Weapon
     public float duration = 3f;
     private float timer;
     public bool secondary = false;
+    public Vector3 direction;
     void Start()
     {
         transform.SetParent(null, true);
         if (!secondary)
         {
-            GetComponent<Rigidbody>().linearVelocity = -transform.right * speed;
+            direction = -transform.right;
         }
         else
         {
-            GetComponent<Rigidbody>().linearVelocity = transform.up * speed;
+            direction = transform.up;
         }
+        Vector3 playerMovementForce = Vector3.zero; 
+        Vector3 playerVelocity = PlayerEquipabbles.S.GetComponent<Rigidbody>().linearVelocity;
+        if (playerVelocity.x * direction.x > 0)
+        {
+            playerMovementForce.x = playerVelocity.x;
+        }
+
+        if (playerVelocity.y * direction.y > 0)
+        {
+            playerMovementForce.y = playerVelocity.y;
+        }
+        if (playerVelocity.z * direction.z > 0)
+        {
+            playerMovementForce.z = playerVelocity.z;
+        }
+
+        GetComponent<Rigidbody>().linearVelocity = direction * speed + playerMovementForce;
     }
 
     void Update()
